e288e39b0b7751fd9708faea343924a5f985b7a1
renzozuk <renzo.zuk19@gmail.com>
2025-02-26 18:52:59 -0300

    Feat: ReactRouter

--- frontend/zuke-store-app/src/App.jsx
+++ frontend/zuke-store-app/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer.jsx";
 import Freight from "./components/Freight.jsx";
 import Header from "./components/Header.jsx";
 import Product from "./components/Product.jsx";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
 
@@ -17,16 +18,7 @@ export default function App() {
           {!showFreight && <button className="show-freight-button" onClick={() => setShowFreight(true)}>Calcular Frete</button>}
           {showFreight && <Freight setShowFreight={setShowFreight} />}
         </div>
-        <div className="products">
-          <Product image="https://images.pexels.com/photos/2815377/pexels-photo-2815377.jpeg" name="Ursinho de Pelúcia" description="Feito com materiais macios e de alta qualidade, ele é incrivelmente fofo ao toque, ideal para abraços aconchegantes." price={49.99} />
-          <Product image="https://images.pexels.com/photos/10160241/pexels-photo-10160241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" name="Globo Terrestre" description="O globo terrestre é uma representação tridimensional do planeta Terra, que ilustra de forma precisa a sua forma esférica e suas características geográficas." price={124.99} />
-          <Product image="https://images.pexels.com/photos/296158/pexels-photo-296158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" name="Sapatos" description="Este par de sapatos é muito mais do que um simples acessório; é uma expressão de estilo, conforto e funcionalidade." price={399.99} />
-          <Product image="https://images.pexels.com/photos/683624/pexels-photo-683624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" name="Despertador" description="Perfeito para despertar no início da manhã e não perder a hora de ir ao trabalho. Diferentes sons ou músicas podem ser usados para o despertador." price={55.90} />
-          <Product image="https://images.pexels.com/photos/3428498/pexels-photo-3428498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" name="Violão" description="Um instrumento musical de cordas, feito de madeira, com uma caixa de ressonância e braço longo. Suas seis cordas são tocadas com os dedos ou palheta, produzindo sons ricos e harmônicos." price={299.99} />
-          <Product image="https://images.pexels.com/photos/2228889/pexels-photo-2228889.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" name="Xícara" description="Perfeita para tomar um bom café no início da manhã ou no lanche da tarde." price={19.99} />
-          <Product image="https://images.pexels.com/photos/42257/flowerful-flowery-flowerly-42257.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" name="Vaso" description="Perfeito para colocar lindas flores dentro e decorar o ambiente, afim de deixá-lo mais agradável." price={14.99} />
-          <Product image="https://images.pexels.com/photos/984619/pexels-photo-984619.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" name="Chapéu" description="Ideal para se proteger do sol em um dia ensolarado, além de disfarçar as falhas da calvície." price={4999.99} />
-        </div>
+        <Outlet />
       </div>
       <Footer />
     </>
--- frontend/zuke-store-app/src/index.jsx
+++ frontend/zuke-store-app/src/index.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Department from "./pages/Department.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Create the router
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Department />,
+            },
+        ],
+    },
+]);
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
